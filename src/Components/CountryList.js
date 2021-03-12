2d5054a604c713658b5261b0ded819e7b1084c72
Yu Gao <gaoyububest@gmail.com>
2021-03-12 17:28:11 -0500

    Delete debug code

--- src/Components/CountryList.js
+++ src/Components/CountryList.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function CountryList({countries}) {
-  console.log(countries)
   const countryList = countries.map(country => <li key={country.toLowerCase()} className="pa-10 pl-20">{country}</li>);
 
   return (
